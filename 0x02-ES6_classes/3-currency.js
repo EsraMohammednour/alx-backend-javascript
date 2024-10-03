d0c542100d3bd03bf5e0793d371ab9b442ec4e32
esraa <esraa2023112@gmail.com>
2024-10-03 21:42:28 +0300

    done

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -9,9 +9,6 @@ export default class Currency {
   }
 
   set name(newname) {
-    if (typeof newname !== 'string') {
-      throw new Error('Name must be a string');
-    }
     this._name = newname;
   }
 
@@ -20,9 +17,6 @@ export default class Currency {
   }
 
   set code(newcode) {
-    if (typeof newcode !== 'string') {
-      throw new Error('Code must be a string')
-    }
     this._code = newcode;
   }
 
